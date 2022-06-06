08f8eea525adff7575c40a29af1aa36c89e8fff9
Zack Longboy <zalongboy@Zacks-MBP.attlocal.net>
2022-06-06 06:32:46 -0400

    push main as backup

--- final-project/tents/main.js
+++ final-project/tents/main.js
@@ -1,36 +1,35 @@
+
 //Get all carousel slides
 const carouselSlides = document.querySelectorAll(".carousel__slide");
 carouselSlides.forEach(function(carouselSlide, index) {
     carouselSlide.style.transform = `translateX(${index*100}%)`
 })
 
-//next button
-const nextSlide = document.querySelector(".carousel__btn--next")
-let currentSlide = 0
+//next slide
+const nextSlide = document.querySelector(".carousel__btn--next");
+let currentSlide = 0;
 
 nextSlide.addEventListener('click', function() {
     if (currentSlide === carouselSlides.length-1) {
         currentSlide = 0;
     }
     else currentSlide++;
-    console.log(currentSlide);
     carouselSlides.forEach(function(carouselSlide, index) {
-        carouselSlide.style.transform = `translateX(${100*(index - currentSlide)}%)`
-        carouselSlide.style.transition = 'all 1000ms'
+        carouselSlide.style.transform = `translateX(${100*(index - currentSlide)}%)`;
+        carouselSlide.style.transition = 'all 1000ms';
     });
 })
 
-//prev button
-const prevSlide = document.querySelector(".carousel__btn--prev")
-
+//prev slide
+const prevSlide = document.querySelector(".carousel__btn--prev");
 prevSlide.addEventListener('click', function() {
     if (currentSlide === 0) {
         currentSlide = carouselSlides.length - 1;
     }
     else currentSlide--;
-    console.log(currentSlide);
     carouselSlides.forEach(function(carouselSlide, index) {
-        carouselSlide.style.transform = `translateX(${100*(index - currentSlide)}%)`
-        carouselSlide.style.transition = 'all 1000ms'
+        carouselSlide.style.transform = `translateX(${100*(index - currentSlide)}%)`;
+        carouselSlide.style.transition = 'all 1000ms';
+        //TODO: flash of blank white if prevSlide is first click
     });
 })
