5c53c2a1f650a8eff9809fbd37a4b2b322e7a354
Zack Longboy <zalongboy@Zacks-MBP.attlocal.net>
2022-06-07 09:43:50 -0400

    add structure for nav

--- final-project/tents/main.js
+++ final-project/tents/main.js
@@ -1,35 +1,42 @@
-
-//Get all carousel slides
-const carouselSlides = document.querySelectorAll(".carousel__slide");
-carouselSlides.forEach(function(carouselSlide, index) {
-    carouselSlide.style.transform = `translateX(${index*100}%)`
-})
-
-//next slide
-const nextSlide = document.querySelector(".carousel__btn--next");
-let currentSlide = 0;
-
-nextSlide.addEventListener('click', function() {
-    if (currentSlide === carouselSlides.length-1) {
-        currentSlide = 0;
-    }
-    else currentSlide++;
+//Carousel
+(function() {
+    //Get all slides
+    const carouselSlides = document.querySelectorAll(".carousel__slide");
     carouselSlides.forEach(function(carouselSlide, index) {
-        carouselSlide.style.transform = `translateX(${100*(index - currentSlide)}%)`;
-        carouselSlide.style.transition = 'all 1000ms';
-    });
-})
+        carouselSlide.style.transform = `translateX(${index*100}%)`
+    })
 
-//prev slide
-const prevSlide = document.querySelector(".carousel__btn--prev");
-prevSlide.addEventListener('click', function() {
-    if (currentSlide === 0) {
-        currentSlide = carouselSlides.length - 1;
-    }
-    else currentSlide--;
-    carouselSlides.forEach(function(carouselSlide, index) {
-        carouselSlide.style.transform = `translateX(${100*(index - currentSlide)}%)`;
-        carouselSlide.style.transition = 'all 1000ms';
-        //TODO: flash of blank white if prevSlide is first click
+    //next slide
+    const nextSlide = document.querySelector(".carousel__btn--next");
+    let currentSlide = 0;
+
+    nextSlide.addEventListener('click', function() {
+        if (currentSlide === carouselSlides.length-1) {
+            currentSlide = 0;
+        }
+        else currentSlide++;
+        carouselSlides.forEach(function(carouselSlide, index) {
+            carouselSlide.style.transform = `translateX(${100*(index - currentSlide)}%)`;
+            carouselSlide.style.transition = 'all 1000ms';
+        });
+    })
+
+    //prev slide
+    const prevSlide = document.querySelector(".carousel__btn--prev");
+    prevSlide.addEventListener('click', function() {
+        if (currentSlide === 0) {
+            currentSlide = carouselSlides.length - 1;
+        }
+        else currentSlide--;
+        carouselSlides.forEach(function(carouselSlide, index) {
+            carouselSlide.style.transform = `translateX(${100*(index - currentSlide)}%)`;
+            carouselSlide.style.transition = 'all 1000ms';
+            //TODO: flash of blank/white if prevSlide is first click
+        });
     });
-})
+})()
+
+//Nav
+(function() {
+
+})()
