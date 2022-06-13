792fb5adc3d1b615417fc89d4bf7f351ab93e1e8
Zack Longboy <zalongboy@Zacks-MBP.lan>
2022-06-12 22:18:49 -0400

    product page pulls data from json and constructs product cards. refactored featured product flag

--- final-project/tents/products.js
+++ final-project/tents/products.js
@@ -1,21 +1,72 @@
-//Products
-(function() {
-    var allProducts;
-    const productsURL = '/products.json'
+//***** PRODUCTS *****
+const productsURL = '/products.json';
 
-    async function getProducts(path) {
-        const response = await fetch(path, {
-            method:'GET'
-        })
-        const raw = await response.json()
-        console.log(raw)
-    }
+var getProducts = function(path) { 
+    return fetch(path)
+    .then(response => response.json())
+    .then(function(json) {
+        allProducts = []
+        const { products } = json;
+        allProducts = products;
+        return allProducts;
+    })
+    .catch(error => console.log(error))
+};
 
-    getProducts(productsURL)
-        .catch(error => {
-            console.log(error)
-        });
-})();
-//Sort
 
-//Display
+//***** SORT *****
+//input: raw products
+//listen for input changes (add listeners for every filter): filter based on inputs, if not return default sorting 
+           //use some(array.property) method
+//output: filteredProducts
+
+
+//***** DISPLAY *****
+
+//Helper functions
+var titleInner = function(obj) {
+    return `<b>${obj.brand} </b>${obj.name}`
+}
+
+var imgInner = function(obj) {
+    return `<img src="${obj.image}">`
+}
+
+var summaryInner = function(obj) {
+    return `<div class="product-summary__price"><b>$ </b>${obj.price}</div>
+            <div class="product-summary__capacity"><b>CAPACITY</b><br>${obj.capacity}-person</div>
+            <div class="product-summary__rating"><b>REVIEW</b><br><span class="review-stars">${obj.score} stars</span></div>
+            <a class="product-card__btn"><span>SHOP NOW</span></a>`
+}
+
+function displayElement(name, type, parent, inner, ...classes) {
+    name = document.createElement(type);
+    name.classList.add(classes);
+    name.innerHTML = inner;
+    parent.appendChild(name);
+}
+
+function displayProducts(filteredProducts) {
+    const container = document.querySelector('.product-card__container')
+    filteredProducts.forEach(product => {
+        const fragment = document.createDocumentFragment();
+       
+        const card = document.createElement('div')
+        card.classList.add('product-card');
+        displayElement('imgContainer', 'div', card, imgInner(product), 'product-img__container')
+        displayElement('title', 'div', card, titleInner(product), 'product-title')
+        displayElement('summary', 'div', card, summaryInner(product), 'product-summary__container')
+        if (product.featured) {
+            displayElement('featured', 'div', card, '<span>Top-rated</span>', 'product-featured')
+        }
+        
+        fragment.appendChild(card);
+        container.appendChild(fragment)
+    });
+}
+
+//***** RUN ALL *****
+getProducts(productsURL).then(function(allProducts) {
+    //FINAL: displayProducts(sortProducts(allProducts))
+     displayProducts(allProducts); 
+ });
