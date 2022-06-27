b323a52daf05fd47ef4d3b85b5ad2ac6c3006cd2
zlongboy <zlongboy@me.com>
2022-06-26 22:31:02 -0400

    first refactor of contact form error validation

--- final-project/tents/js/contact.js
+++ final-project/tents/js/contact.js
@@ -5,17 +5,16 @@ mobileNav();
 
 //CONTACT FORM
 const form = document.querySelector('#contact-form');
-const firstName = document.querySelector('input[name="firstName"]');
-const lastName = document.querySelector('input[name="lastName"]');
-const email = document.querySelector('input[name="email"]');
-const phone = document.querySelector('input[name="phone"]');
 
-const firstNameError = document.querySelector('#firstName-error-msg');
-const lastNameError = document.querySelector('#lastName-error-msg');
-const emailError = document.querySelector('#email-error-msg');
-const phoneError = document.querySelector('#phone-error-msg');
+const input = function(inputName) {
+  return document.querySelector(`input[name="${inputName}"]`);
+};
 
-//Define error messages
+const errorContainer = function(inputErrored) {
+  return document.querySelector(`#${inputErrored}-error-msg`);
+};
+
+//ERROR MESSAGES
 const errorMsg = {
   empty: 'Required.',
   nameInvalid: 'Name must only contain letters and spaces.',
@@ -23,18 +22,19 @@ const errorMsg = {
   phoneInvalid: 'Please enter a valid phone number.'
 };
 
-//Helper functions
+//HELPER FUNCTIONS
 function applyErrorStyles(input, div, msg) {
   input.classList.add('error-input');
   div.innerText = msg;
 }
+
 function removeErrorStyles(input, div) {
   input.classList.remove('error-input');
   div.innerText = '';
 }
 
-//Validation functions
-function isValidName(name, input, error) {
+//VALIDATION FUNCTIONS
+const isValidName = function(name, input, error) {
   const nameRegex = /^[a-zA-Z\s]*$/ ; 
   if (!nameRegex.test(name)) {
     applyErrorStyles(input, error, errorMsg.nameInvalid);
@@ -48,7 +48,7 @@ function isValidName(name, input, error) {
   };
 };
 
-function isValidEmail(email, input, error) {
+const isValidEmail = function(email, input, error) {
     const emailRegex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/ ;
     if (!emailRegex.test(email)) {
       applyErrorStyles(input, error, errorMsg.emailInvalid);
@@ -62,28 +62,40 @@ function isValidEmail(email, input, error) {
     }
   }
 
-  function isValidPhone(phone, input, error) {
-    const phoneRegex = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/ ;
-    if (!phoneRegex.test(phone)) {
-      applyErrorStyles(input, error, errorMsg.phoneInvalid);
-    }
-    else if (phone.trim().length < 1) {
-      applyErrorStyles(input, error, errorMsg.empty);
-    } 
-    else {
-      removeErrorStyles(input, error);
-      return true;
-    };
+const isValidPhone = function(phone, input, error) {
+  const phoneRegex = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/ ;
+  if (!phoneRegex.test(phone)) {
+    applyErrorStyles(input, error, errorMsg.phoneInvalid);
+  }
+  else if (phone.trim().length < 1) {
+    applyErrorStyles(input, error, errorMsg.empty);
+  } 
+  else {
+    removeErrorStyles(input, error);
+    return true;
   };
+};
 
-//Run all validation functions on submit
+//EVALUATE
+const checkInput = function(inputName, isValid) {
+  return isValid(input(inputName).value, input(inputName), errorContainer(inputName));
+}
+
+//RUN VALIDATION ON SUBMIT
 form.addEventListener('submit', function(element) {
   element.preventDefault();
-  const firstNameValidation = isValidName(firstName.value, firstName, firstNameError);
-  const lastNameValidation = isValidName(lastName.value, lastName, lastNameError);
-  const emailValidation = isValidEmail(email.value, email, emailError);
-  const phoneValidation = isValidPhone(phone.value, phone, phoneError)
+
+  // if ((checkInput('firstName', isValidName)) && (checkInput('lastName', isValidName)) && (checkInput('email', isValidEmail)) && (checkInput('phone', isValidPhone))) {
+  //   //TODO: Add submit function here (if necessary);
+  // };
+  //REFACTOR OPPURTUNITY: Why does above cause validation to run one at a time?
+
+  let firstNameValidation = checkInput('firstName', isValidName);
+  let lastNameValidation = checkInput('lastName', isValidName);
+  let emailValidation = checkInput('email', isValidEmail);
+  let phoneValidation = checkInput('phone', isValidPhone);
+
   if (lastNameValidation && firstNameValidation && emailValidation && phoneValidation) {
-  //TODO: Add submit function here (if necessary)
+    //TODO: Add submit function here (if necessary);
   };
 });
